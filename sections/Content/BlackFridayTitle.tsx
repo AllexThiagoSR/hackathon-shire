1737603b1b43ff22453286854a0e4071a8cd5e8d
rnogara <r.nogara.dev@gmail.com>
2023-11-02 17:12:13 -0300

    Add neon lights and black friday title

---
+++ sections/Content/BlackFridayTitle.tsx
@@ -0,0 +1,8 @@
+export default function BlackFriday() {
+   return (
+      <div class='w-full container py-8 flex flex-col lg:py-10'>
+        <h1 class="rotate-330 animation-neon-yellow-text neon-yellow-text text-center text-5xl pe-10">Black</h1>
+        <h1 class="rotate-330 animation-neon-yellow-text neon-yellow-text text-center text-5xl">Friday</h1>
+      </div>
+   );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,20 @@ export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: { container: { center: true },
+    extend: {
+      colors: {
+        'neon-yellow-text': '#f8ffbb',
+        'neon-green-text': '#a7ffe2',
+        'neon-red-text': '#ffa5a5',
+      },
+      fontSize: {
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      rotate: {
+        '330': '-30deg',
+      }
+    }
+  }
 };
